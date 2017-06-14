6a390edf5232b98b1db4082aa99c783273cd7a80
yushao <yushao@windows10.microdone.cn>
2017-06-14 17:38:01 +0800

    Test jenkins console output

--- UpdateUrl/src/UpdateUrl.py
+++ UpdateUrl/src/UpdateUrl.py
@@ -3,41 +3,25 @@ Created on 2017-6-14
 
 @author: yushao
 '''
-# import urllib2
-# import os
-# 
-# dict1 = {'Pmtappsvc2Pgwpmtsoa': os.environ["Pmtappsvc2Pgwpmtsoa"]}
-# dict2 = {'Pmtappsvc2Pgwsecdas': os.environ["Pmtappsvc2Pgwsecdas"]}
-# dicts = [dict1['Pmtappsvc2Pgwpmtsoa'] , dict2['Pmtappsvc2Pgwsecdas']]
-# 
-# for di in dicts:
-#   
-#   url = di
-#   print (url)
-#   
-#   response=urllib2.urlopen(url)
-#   
-#   print(response.info())   
-#   print(response.geturl())  
-#   print(response.getcode())
+import urllib2
+import os
+import sys
 
-import urllib.request
-# import os
-   
-dict = {'pmtappsvc': 'http://monterey-9092.slc07.dev.ebayc3.com:8080/admin/v3console', 'pgwsecdas': 'http://monterey-5179.slc01.dev.ebayc3.com:8080/pgwpmtsoa/PgwServiceV1'}
-# dict = {'pmtappsvc': 'http://monterey-9092.slc07.dev.ebayc3.com:8080/admin/v3console', 'pgwsecdas': 'http://pgwsecdas-stg.stratus.qa.ebay.com/pgwsecdas/v1'}
- 
-# dict = {'pmtappsvc': $pmtappsvc, 'pgwsecdas': $pgwsecdas}
-url = dict['pmtappsvc']+"/UpdateConfigCategoryXml?"+"id=org.ebayopensource.ginger.client.pgwdas.staging.PGWDasClient&ENDPOINT_URI="+dict['pgwsecdas']
- 
- 
-print (url)
 
-response=urllib.request.urlopen(url)
- 
+dict1 = {'Pmtappsvc2Pgwpmtsoa': os.environ["Pmtappsvc2Pgwpmtsoa"]}
+dict2 = {'Pmtappsvc2Pgwsecdas': os.environ["Pmtappsvc2Pgwsecdas"]}
+dicts = [dict1['Pmtappsvc2Pgwpmtsoa'] , dict2['Pmtappsvc2Pgwsecdas']]
 
-print(response.info()) 
-
-print(response.geturl())
- 
-print(response.getcode())
+for di in dicts:
+    if "not update" in di:
+        continue
+    else: 
+      url = di
+      print (url)
+      
+      response=urllib2.urlopen(url)
+      
+      print(response.info())   
+      print(response.geturl())  
+      print(response.getcode())
+      sys.stdout.flush()
